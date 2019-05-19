79a56452abea94159f29443bcddbf6357995c167
Zach Parker <parkerz410@gmail.com>
2019-05-19 13:56:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,12 @@
 #write your code here
 
-def countdown (x)
-  x = 10
-  while x > 0
-    puts "#{x} SECOND(S)!"
-    x -= 1
+def countdown (n)
+  n = 10
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
     end
-    if x <= 0
+    if n <= 0
       puts "HAPPY NEW YEAR!"
     end
 end
